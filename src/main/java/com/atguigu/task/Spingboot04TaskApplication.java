6e5bcf5a0684aa09469442efa600f581bead685f
jason-deng-hub <154164587@qq.com>
2020-02-11 14:20:27 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/task/Spingboot04TaskApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spingboot04TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spingboot04TaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atguigu/task/Spingboot04TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spingboot04TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
